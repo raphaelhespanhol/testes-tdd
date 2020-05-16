d3197e5dcd9eb64f24a5403f5306f893c1def80d
Raphael Hespanhol <rhespanhol@ciandt.com>
2020-05-16 14:29:11 -0300

    Added the project files

---
+++ src/test/java/com/brothers/testestdd/TestesTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brothers.testestdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
