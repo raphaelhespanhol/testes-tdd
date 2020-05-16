d59b09e04a28b86abb905942f6a0e285d0034e8b
Raphael Hespanhol <rhespanhol@ciandt.com>
2020-05-16 15:33:52 -0300

    Added a new structure

---
+++ src/test/java/com/brothers/testtdd/TestTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brothers.testtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
