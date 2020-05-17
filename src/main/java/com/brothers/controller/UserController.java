674e46b22d70ccca7a7be19d540b08a0762d9a0d
Raphael Hespanhol <rhespanhol@ciandt.com>
2020-05-17 18:34:02 -0300

    Added a file with all messages

--- src/main/java/com/brothers/controller/UserController.java
+++ src/main/java/com/brothers/controller/UserController.java
@@ -1,30 +1,29 @@
 package com.brothers.controller;
 
 import com.brothers.controller.dto.UserDTO;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.net.URI;
 
-@Controller
+@RestController
 @RequestMapping("api/users")
 public class UserController {
 
     @PostMapping
-    public ResponseEntity create(@Valid @RequestBody UserDTO user){
+    public ResponseEntity create(@RequestBody @Valid UserDTO user){
         return ResponseEntity.created(URI.create("/id?".concat("1"))).body(user);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+//        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+//    }
 }
 
 
--- src/main/java/com/brothers/controller/dto/UserDTO.java
+++ src/main/java/com/brothers/controller/dto/UserDTO.java
@@ -3,15 +3,21 @@ package com.brothers.controller.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-    @NotNull(message = "Name cannot be null")
+    @NotNull(message = "{user.name.notNull}")
+    @Size(min=3, max=120, message = "{user.name.size}")
     private String name;
 
-    @NotNull(message = "Age cannot be null")
+    @NotNull(message = "{user.age.notNull}")
+    @Positive
     private Integer age;
 }
